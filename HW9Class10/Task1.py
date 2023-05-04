1892a32da0c4859f717d453c0b6ee9e25249941b
kzhdanova <kzhdanova@architech.ca>
2023-05-04 12:01:55 +0300

    HW9Class10 added

---
+++ HW9Class10/Task1.py
@@ -0,0 +1,18 @@
+# Write a function called oops that explicitly raises an IndexError exception
+# when called. Then write another function that calls oops inside a try/except
+# state­ment to catch the error. What happens if you change oops to
+# raise KeyError instead of IndexError?
+
+def oops():
+    raise IndexError
+
+
+def test():
+    try:
+        oops()
+    except IndexError :
+        print('IndexError occured')
+
+
+test()
+
